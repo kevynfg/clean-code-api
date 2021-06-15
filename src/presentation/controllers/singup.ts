6179bfd166f509c94c77beb73cc51c92090eafec
kevyn gonçalves <kevynredlotus@gmail.com>
2021-06-15 11:38:34 -0300

    test email should return 400 if no email is provided

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -2,6 +2,9 @@ export class SignUpController {
     handle (httpRequest: any): any {
         const request = httpRequest;
         if (!request.body.name) return {statusCode: 400, body: new Error('Missing param: name')};
+
+        if (!request.body.email) return {statusCode: 400, body: new Error('Missing param: email')};
+        
         return {statusCode: 200};
     }
 }
