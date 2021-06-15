0b8eb6ec00da4a32e0c4e3373100ba4407cd1d2e
kevyn gonçalves <kevynredlotus@gmail.com>
2021-06-15 12:04:04 -0300

    added test for password

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,5 @@
 import { SignUpController } from "./singup";
+import { MissingParamError } from "../errors/missing-param-error";
 
 describe('SignUp Controller', () => {
     test('should return 400 if no name is provided', () => {
@@ -13,7 +14,7 @@ describe('SignUp Controller', () => {
         }
         const httpResponse = sut.handle(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
-        expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+        expect(httpResponse.body).toEqual(new MissingParamError('name'));
     })
 
     test('should return 400 if no email is provided', () => {
@@ -28,6 +29,21 @@ describe('SignUp Controller', () => {
         }
         const httpResponse = sut.handle(httpRequest);
         expect(httpResponse.statusCode).toBe(400);
-        expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+        expect(httpResponse.body).toEqual(new MissingParamError('email'));
+    })
+
+    test('should return 400 if no password is provided', () => {
+        //sut -> system under test é uma nomenclatura padrão para classes em teste
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@email.com',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new MissingParamError('password'));
     })
 })
