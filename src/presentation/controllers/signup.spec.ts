3d8ae4602586dbbeabc6519c859f0d91f660dfca
kevyn gonçalves <kevynredlotus@gmail.com>
2021-06-15 11:06:14 -0300

    added test and mocks for test

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from "./singup";
+
+describe('SignUp Controller', () => {
+    test('should return 400 if no name is provided', () => {
+        //sut -> system under test é uma nomenclatura padrão para classes em teste
+        const sut = new SignUpController();
+        const httpRequest = {
+            body: {
+                email: 'any_email@email.com',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest);
+        expect(httpResponse.statusCode).toBe(400);
+        expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+    })
+})
--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,7 +1,7 @@
 export class SignUpController {
     handle (httpRequest: any): any {
         const request = httpRequest;
-        if (!request.body.name) return {statusCode: 400};
+        if (!request.body.name) return {statusCode: 400, body: new Error('Missing param: name')};
         return {statusCode: 200};
     }
 }
