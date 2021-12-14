35c9f6293e8b1fe7058b13174acfe7ebea72b1b7
Kevyn Gonçalves <kevynredlotus@gmail.com>
2021-12-14 11:17:32 -0300

    feat: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from "./singup";
 import { MissingParamError } from "../errors/missing-param-error";
 import { InvalidParamError } from "../errors/invalid-param-error";
 import { EmailValidator } from "../protocols/email-validator";
+import { ServerError } from "../errors/server-error";
 
 interface SutTypes {
   sut: SignUpController;
@@ -39,7 +40,6 @@ describe("SignUp Controller", () => {
   });
 
   test("should return 400 if no email is provided", () => {
-    //sut -> system under test é uma nomenclatura padrão para classes em teste
     const { sut } = makeSut();
     const httpRequest = {
       body: {
@@ -54,7 +54,6 @@ describe("SignUp Controller", () => {
   });
 
   test("should return 400 if no password is provided", () => {
-    //sut -> system under test é uma nomenclatura padrão para classes em teste
     const { sut } = makeSut();
     const httpRequest = {
       body: {
@@ -69,7 +68,6 @@ describe("SignUp Controller", () => {
   });
 
   test("if no passwordConfirmation is provided", () => {
-    //sut -> system under test é uma nomenclatura padrão para classes em teste
     const { sut } = makeSut();
     const httpRequest = {
       body: {
@@ -86,7 +84,6 @@ describe("SignUp Controller", () => {
   });
 
   test("should return 400 if an invalid email is provided", () => {
-    //sut -> system under test é uma nomenclatura padrão para classes em teste
     const { sut, emailValidatorStub } = makeSut();
     jest.spyOn(emailValidatorStub, "isValid").mockReturnValueOnce(false);
     const httpRequest = {
@@ -100,5 +97,41 @@ describe("SignUp Controller", () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParamError("email"));
+  });
+
+  test("should call EmailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com');
+  });
+
+  test("should return 500 EmailValidator throws", () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid(email: string): boolean {
+        throw new Error();
+      }
+    }
+    const emailValidatorStub = new EmailValidatorStub();
+    const sut = new SignUpController(emailValidatorStub);
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
   });
 });
