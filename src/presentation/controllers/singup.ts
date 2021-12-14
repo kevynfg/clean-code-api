38ea2501a752b15a9c50d8448ede030b7a551a11
Kevyn Gonçalves <kevynredlotus@gmail.com>
2021-12-14 11:22:17 -0300

    refactor: move all error imports to a single file

--- src/presentation/controllers/singup.ts
+++ src/presentation/controllers/singup.ts
@@ -1,10 +1,8 @@
-import { MissingParamError } from "../errors/missing-param-error";
 import { HttpRequest, HttpResponse } from "../protocols/http";
-import { badRequest } from "../helpers/http-helper";
+import { badRequest, serverError } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
-import { InvalidParamError } from "../errors/invalid-param-error";
-import { ServerError } from "../errors/server-error";
+import { InvalidParamError, MissingParamError } from "../errors/";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -29,11 +27,8 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError("email"));
       }
     } catch (error) {
-      console.log(error)
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      console.error(error)
+      return serverError()
     }
   }
 }
