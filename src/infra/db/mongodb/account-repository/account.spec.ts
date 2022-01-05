fd814cfdd944f726bb66bbfabda0cccdb880ae92
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-01-05 16:20:23 -0300

    add: mongoHelper to ensure it connects to mongodb for testing

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,24 @@
+import { MongoHelper } from "../helpers/mongo-helper";
+describe("Account Mongo Repository", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  test("Should return an account on success", async () => {
+    const sut = new AccountMongoRepository();
+    const account = await sut.add({
+      name: "valid_name",
+      email: "valid_email@email.com",
+      password: "valid_password",
+    });
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe("valid_name");
+    expect(account.email).toBe("valid_email@email.com");
+    expect(account.password).toBe("valid_password");
+  });
+});
