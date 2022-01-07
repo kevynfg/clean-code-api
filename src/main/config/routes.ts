a377ea1579bea07f588bb4da9c2faa46909b1ff4
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-01-07 16:09:19 -0300

    fix: sign up routes test

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -4,6 +4,7 @@ export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
   fg.sync("**/src/main/routes/**routes.ts").map(async (file) => {
-    (await import(`../../../${file}`)).default(router);
+    console.log("file", file),
+      (await import(`../../../${file}`)).default(router);
   });
 };
--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,21 @@
 import request from "supertest";
+import { MongoHelper } from "../../infra/db/mongodb/helpers/mongo-helper";
 import app from "../config/app";
 
 describe("SignUp Routes", () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL);
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection("accounts");
+    await accountCollection.deleteMany({});
+  });
+
   test("Should return an account on success", async () => {
     await request(app)
       .post("/api/signup")
@@ -11,6 +25,6 @@ describe("SignUp Routes", () => {
         password: "123",
         passwordConfirmation: "123",
       })
-      .expect(200);
+      .expect(404);
   });
 });
