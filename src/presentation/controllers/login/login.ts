e12a5264a6b45cafd7fa31314a54b46a4004b31d
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-01-13 11:00:05 -0300

    test: ensure LoginController return 400 if no email or password is provided for tests

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,14 @@
+import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers/http-helper";
+import { Controller, HttpRequest, HttpResponse } from "../../protocols";
+
+export class LoginController implements Controller {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.email) {
+      return Promise.resolve(badRequest(new MissingParamError("email")));
+    }
+    if (!httpRequest.body.password) {
+      return Promise.resolve(badRequest(new MissingParamError("password")));
+    }
+  }
+}
