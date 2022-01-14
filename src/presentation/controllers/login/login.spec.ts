a899ef2f8b1878770c241c092f1c64abd76a1704
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-01-14 11:12:22 -0300

    feat: ensure LoginController returns unauthorized if invalid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,6 +1,10 @@
 import { Authentication } from "../../../domain/usecases/authentication";
 import { InvalidParamError, MissingParamError } from "../../errors";
-import { badRequest, serverError } from "../../helpers/http-helper";
+import {
+  badRequest,
+  serverError,
+  unauthorized,
+} from "../../helpers/http-helper";
 import { EmailValidator, HttpRequest } from "../signup/signup-protocols";
 import { LoginController } from "./login";
 
@@ -104,5 +108,15 @@ describe("Login Controller", () => {
       "any_email@email.com",
       "valid_password"
     );
+  });
+
+  test("Should return 401 if invalid credentials are provided", async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest
+      .spyOn(authenticationStub, "auth")
+      .mockReturnValueOnce(Promise.resolve(null));
+
+    const httpResponse = await sut.handle(makeFakeLoginRequest());
+    expect(httpResponse).toEqual(unauthorized());
   });
 });
