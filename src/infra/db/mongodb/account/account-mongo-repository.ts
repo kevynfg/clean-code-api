7ce4caaceb71d558781cdf505d2df2d23a663684
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-05-02 20:09:48 -0300

    ajuste login

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,12 +1,24 @@
-import { InsertOneResult } from "mongodb";
-import { AddAccountModel } from "../../../../domain/usecases/add-account";
-import { MongoHelper } from "../helpers/mongo-helper";
+import { InsertOneResult, ObjectId } from 'mongodb'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
 export class AccountMongoRepository {
   async add(accountData: AddAccountModel): Promise<InsertOneResult<Document>> {
-    const accountCollection = await MongoHelper.getCollection("accounts");
-    const result = await accountCollection.insertOne(accountData);
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
     return new Promise((resolve) => {
-      resolve(result);
-    });
+      resolve(result)
+    })
+  }
+  async load(email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = (await accountCollection.findOne({ email })) as any as AccountModel
+    return new Promise((resolve) => {
+      resolve(account)
+    })
+  }
+  async update(id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.updateOne({ _id: new ObjectId(id) }, { $set: { token } })
   }
 }
