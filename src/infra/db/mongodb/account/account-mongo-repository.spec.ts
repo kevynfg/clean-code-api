657bdb14d446ec31b6e1fa845043291587dae41a
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-05-10 19:01:12 -0300

    refactor routes file

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -1,32 +1,32 @@
-import { MongoHelper } from "../helpers/mongo-helper";
-import { AccountMongoRepository } from "./account-mongo-repository";
+import { MongoHelper } from '../helpers/mongo-helper'
+import { AccountMongoRepository } from './account-mongo-repository'
 
-describe("Account Mongo Repository", () => {
+describe('Account Mongo Repository', () => {
   beforeAll(async () => {
-    await MongoHelper.connect(process.env.MONGO_URL);
-  });
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
 
   afterAll(async () => {
-    await MongoHelper.disconnect();
-  });
+    await MongoHelper.disconnect()
+  })
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
-    await accountCollection.deleteMany({});
-  });
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
 
   const makeSut = (): AccountMongoRepository => {
-    return new AccountMongoRepository();
-  };
+    return new AccountMongoRepository()
+  }
 
-  test("Should return an account on success", async () => {
-    const sut = makeSut();
+  test('Should return an account on success', async () => {
+    const sut = makeSut()
     const account = await sut.add({
-      name: "valid_name",
-      email: "valid_email@email.com",
-      password: "valid_password",
-    });
-    expect(account).toBeTruthy();
-    expect(account.insertedId).toBeTruthy();
-  });
-});
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password',
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+  })
+})
