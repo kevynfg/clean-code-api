70671dd4bf197924a027dffda9bcdfb333e9ab5f
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-05-24 22:44:54 -0300

    add: new test to throw if authentication throws

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -131,4 +131,12 @@ describe("SignUp Controller", () => {
       password: "valid_password",
     });
   });
+
+  test("Should return 500 if Authentication throws", async () => {
+    const { sut, authenticationStub } = makeSut();
+    jest.spyOn(authenticationStub, "auth").mockReturnValueOnce(Promise.reject(new Error()));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
 });
